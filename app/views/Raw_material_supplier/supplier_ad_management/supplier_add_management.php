6407c328ccc32acce97981e0c97a498bbe3d1747
Naveekj <92240937+Naveekj@users.noreply.github.com>
2023-02-25 20:49:15 +0530

    add urlroot for form and name attribute for input

--- app/views/Raw_material_supplier/supplier_ad_management/supplier_add_management.php
+++ app/views/Raw_material_supplier/supplier_ad_management/supplier_add_management.php
@@ -22,7 +22,7 @@
           <div class="search_bar">
               <div class="search_content">
                   
-                      <span class="search_cont" onclick="open_cansel_btn()"><input type="text" name="search_text" placeholder="<?php  echo $_SESSION['search_cont']?> " require/></span>
+                      <span class="search_cont" onclick="open_cansel_btn()"><input type="text" name="search_text" placeholder="" " require/></span>
                       <button type="submit" class="search_btn" onclick="clear_search_bar()" value=""><i class="fa-solid fa-xmark" id="cansel" ></i></button>
                       <button type="submit" class="search_btn"><i class="fa fa-search" aria-hidden="true" id="search"></i></button>
                   
@@ -35,43 +35,40 @@
           </div>
         </div>
 
-                <div class = "filter_and_add">
+                <!-- <div class = "filter_and_add">
                         <div class="sm_filter_section">
                                 <label for="ongoing_progress__order" id="sm_filter_label"> <input type="radio" id="ongoing_progress" name="order_type" value="ongoing" checked>Pending</label>
                                 <label for="ongoing_ready_order" id="sm_filter_label"> <input type="radio" id="ongoing_ready" name="order_type" value="ongoing">Complete</label>
                                 <label for="cancel_order" id="sm_filter_label"><input type="radio" id="cancel" name="order_type" value="cancel">Cancel</label>
                         </div>
-                </div>
+                </div> -->
                 <div class="sm_view_list">
                 <div class="views">
 
                 <table class="sm_view_list_table">
                         <tr class="table_head">
                                 <td>Image</td>
-                                <td>Title</td>
-                                <td>Category</td>
-                                <td>Certificate No</td>
-                                <td>Manufacture</td>
-                                <td>Quantity</td>
-                                <td>Price</td>
+                                <td>Name</td>
+                                <td>Price(Rs.)</td>
+                                <td>Avaiable Quantity</td>
+                                <td>Additional Infomation</td>
                                 <td>Options</td>
                                 <td></td>
                         </tr>
 
+                        <?php foreach($data['posts'] as $ad): ?>
                         <tr class="sm_view">
                                 <div class="sm_view_detail">
                                         <td><img src="../public/images/other_image_2.jpg" alt="Girl in a jacket" id="fertilizer_img" ></td>
-                                        <td><span class="title">Paddy fertilizer</span></td>
-                                        <td><span class="category">Paddy</span></td>
-                                        <td><span class="certificate No<">PD0001</span></td>
-                                        <td><span class="manufacture">ABC producers</span></td>
-                                        <td class="quantity"><span class="value">4</span></td>
-                                        <td><span class="price">Rs. 1000 x 4</span></td>
+                                        <td><span class="title"><?php echo $ad->product_name ?></span></td>
+                                        <td><span class="category"><?php echo $ad->price ?></span></td>
+                                        <td><span class="certificate No<"><?php echo $ad->quantity ?> ml</span></td>
+                                        <td><span class="manufacture"><?php echo $ad->product_description ?></span></td>
 
 
                                         <td id="option">
                                                 
-                                                <span class="edit"><i class="fa-solid fa-pen-to-square"></i></span>
+                                                <span class="edit"><a href="<?php echo URLROOT?>/supplier_ad_management/update_advertisment/<?php echo $ad->Product_id ?>"><i class="fa-solid fa-pen-to-square"></i></a></span>
                                                 
                                                 
                                                 <span class="delete"><i class="fa-solid fa-trash-can"></i></span>
@@ -80,17 +77,15 @@
                                 </div>
 
                         </tr>
+                        <?php endforeach; ?>
 
-                        <tr class="sm_view">
+                        <!-- <tr class="sm_view">
                                 <div class="sm_view_detail">
                                         <td><img src="../public/images/background2.jpg" alt="Girl in a jacket" id="fertilizer_img" ></td>
                                         <td><span class="title">Paddy fertilizer</span></td>
                                         <td><span class="category">Paddy</span></td>
                                         <td><span class="certificate No<">PD0001</span></td>
                                         <td><span class="manufacture">ABC producers</span></td>
-                                        <td class="quantity"><span class="value">4</span></td>
-                                        <td><span class="price">Rs. 1000 x 4</span></td>
-
 
                                         <td id="option">
                                                 
@@ -110,9 +105,6 @@
                                         <td><span class="category">Paddy</span></td>
                                         <td><span class="certificate No<">PD0001</span></td>
                                         <td><span class="manufacture">ABC producers</span></td>
-                                        <td class="quantity"><span class="value">4</span></td>
-                                        <td><span class="price">Rs. 1000 x 4</span></td>
-
 
                                         <td id="option">
                                                 
@@ -123,7 +115,7 @@
                                                 
                                         </td>
                                 </div>
-                                </tr>
+                                </tr> -->
                                 
                 </table>
 
