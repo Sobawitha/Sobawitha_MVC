cb08ab52f9c6a2b1e968e131c77a50ccf0f583bc
Devin Yapa <72931352+Devinyapa@users.noreply.github.com>
2023-04-17 16:23:42 +0530

    do some changes to supplier sidebar and top nav bar

--- app/views/Raw_material_supplier/Raw_material_supplier/supplier_sidebar.php
+++ app/views/Raw_material_supplier/Raw_material_supplier/supplier_sidebar.php
@@ -1,6 +1,12 @@
 <!--sidebar-->
 <link rel="stylesheet" href="../css/Users/component/sidebar.css"></link>
 <script src="../js/component/sidebar.js"></script> 
+
+<?php
+// Get the current URL
+$current_url = "http".(!empty($_SERVER['HTTPS'])?"s":"")."://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+?>
+
 <div class="sidebar">
     <div class="logo">
         <i class="fa-solid fa-leaf" id="leaf"></i>
@@ -8,21 +14,22 @@
         <i class="fa-solid fa-bars" id="equal"></i>
         <i class="fa-solid fa-play" id="shape"></i>
     </div>
-    <img  src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($_SESSION['profile_image']);?>" id="userprofileimage"/>
+    <img src="<?php echo URLROOT ?>/public/upload/user_profile_pics/<?php echo $_SESSION['profile_image']?>"   alt="Profile Picture"  id="userprofileimage" />
      <div class="user_detail">
      <span class="uname"> <?php echo $_SESSION['username'] ," ",$_SESSION['lastname'] ?>  </span><br>
     <span class="position"><?php echo $_SESSION['position'] ?></span>
     </div>
     <ul>
-        <li><a href="<?php echo URLROOT?>/Pages/home"><i class="fa-solid fa-house"></i>&nbsp;&nbsp;Home</a></li>
-        <li><a href="<?php echo URLROOT?>/resources/resource_page"><i class="fa-solid fa-file-signature"></i>&nbsp;&nbsp;Resources</a></li>
-        <li><a href="<?php echo URLROOT?>/forum/forum"><i class="fa-brands fa-forumbee"></i>&nbsp;&nbsp;Forum</a></li>
-        <li><a href="<?php echo URLROOT?>/dashboard/supplier_dashboard" class=""><i class="fas fa-table"></i>&nbsp;&nbsp;Dashboard</a></li>
-        <li><a href="<?php echo URLROOT?>/order/supplier_order_list" class=""><i class="fa-solid fa-list-check"></i>&nbsp;&nbsp;View orderlist</a></li>
-        <li><a href="<?php echo URLROOT?>/sales/sales_list" class=""><i class="fa-solid fa-list"></i>&nbsp;&nbsp;View sales</a></li>
-        <li><a href="<?php echo URLROOT?>/complaint/display_all_complaint" class=""><i class="fas fa-table"></i>&nbsp;&nbsp;Complaint</a></li>
-        <li><a href="<?php echo URLROOT?>/feedback/supplire_feedback" class=""><i class="fa-regular fa-comments"></i>&nbsp;&nbsp;Feedback</a></li>
-        <li><a href="<?php echo URLROOT?>/supplier_ad_management/view_advertisment" class=""><i class="fa-solid fa-gear"></i>&nbsp;&nbsp;Manage ads</a></li>
+        <li class="<?php if (strpos($current_url, URLROOT.'/Pages/home') !== false) echo 'active'; ?>"><a href="<?php echo URLROOT?>/Pages/home"><i class="fa-solid fa-house"></i>&nbsp;&nbsp;Home</a></li>
+        <li class="<?php if (strpos($current_url, URLROOT.'/resources') !== false) echo 'active'; ?>"><a href="<?php echo URLROOT?>/resources/resource_page"><i class="fa-solid fa-file-signature"></i>&nbsp;&nbsp;Resources</a></li>
+        <li class="<?php if (strpos($current_url, URLROOT.'/forum') !== false) echo 'active'; ?>"><a href="<?php echo URLROOT?>/forum/forum"><i class="fa-brands fa-forumbee"></i>&nbsp;&nbsp;Forum</a></li>
+        <li class="<?php if (strpos($current_url, URLROOT.'/dashboard') !== false) echo 'active'; ?>"><a href="<?php echo URLROOT?>/dashboard/supplier_dashboard" class=""><i class="fas fa-table"></i>&nbsp;&nbsp;Dashboard</a></li>
+        <li class="<?php if (strpos($current_url, URLROOT.'/supplier_ad_management') !== false) echo 'active'; ?>"><a href="<?php echo URLROOT?>/supplier_ad_management/view_advertisment" class=""><i class="fa-solid fa-gear"></i>&nbsp;&nbsp;Manage ads</a></li>
+        <li class="<?php if (strpos($current_url, URLROOT.'/order') !== false) echo 'active'; ?>"><a href="<?php echo URLROOT?>/order/supplier_order_list" class=""><i class="fa-solid fa-list-check"></i>&nbsp;&nbsp;View orderlist</a></li>
+        <li class="<?php if (strpos($current_url, URLROOT.'/sales') !== false) echo 'active'; ?>"><a href="<?php echo URLROOT?>/sales/sales_list" class=""><i class="fa-solid fa-list"></i>&nbsp;&nbsp;View sales</a></li>
+        <li class="<?php if (strpos($current_url, URLROOT.'/complaint') !== false) echo 'active'; ?>"><a href="<?php echo URLROOT?>/complaint/display_all_complaint" class=""><i class="fas fa-table"></i>&nbsp;&nbsp;Complaint</a></li>
+        <li class="<?php if (strpos($current_url, URLROOT.'/feedback') !== false) echo 'active'; ?>"><a href="<?php echo URLROOT?>/feedback/supplier_feedback" class=""><i class="fa-regular fa-comments"></i>&nbsp;&nbsp;Feedback</a></li>
+        
     </ul>
 </div>
 
--- app/views/Raw_material_supplier/Raw_material_supplier/supplier_topnavbar.php
+++ app/views/Raw_material_supplier/Raw_material_supplier/supplier_topnavbar.php
@@ -3,6 +3,12 @@
 <link rel="stylesheet" href="../css/Users/component/topnavbar.css"></link>
 <script src="../js/Users/component/topnavbar.js"></script> 
 
+<?php
+// Get the current URL
+$current_url = "http".(!empty($_SERVER['HTTPS'])?"s":"")."://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+?>
+
+
 <div class="topnav" id="navbar">
   
 
@@ -37,9 +43,9 @@
 
     <span class="site_name_nav" id="part_a_nav"><i class="fa-solid fa-leaf" id="leaf1"></i></i>SOBA</span><span id="part_b_nav">WITHA</span>
     <div class="nav-link">
-      <a href="<?php echo URLROOT?>/Users/supplier_view_profile">Profile</a>
-      <a href="<?php echo URLROOT?>/dashboard/supplier_dashboard">Dashboard</a>
-      <a href="<?php echo URLROOT?>/order/view_cart" id="cart_icon"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a> <!--change-->
+      <a href="<?php echo URLROOT?>/Supplier/profile" class="<?php if ($current_url === URLROOT.'/Supplier/updateProfile' || $current_url === URLROOT.'/Supplier/profile' || $current_url === URLROOT.'/Users/changePW') echo 'active'; ?>">Profile</a>
+      <a href="<?php echo URLROOT?>/dashboard/supplier_dashboard" class="<?php if ($current_url === URLROOT.'/dashboard/supplier_dashboard') echo 'active'; ?>">Dashboard</a>
+      <!-- <a href="<?php echo URLROOT?>/order/view_cart" id="cart_icon"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a> change -->
       <i class="fa fa-solid fa-bell" id="bell" onclick="openNotificationMenu()"></i>
       <a href="<?php echo URLROOT?>/Login/logout"><i class="fa-solid fa-right-from-bracket" id="dots"></i></a>
     </div>
