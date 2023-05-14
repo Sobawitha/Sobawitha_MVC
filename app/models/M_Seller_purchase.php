a35f531d5319659a40488a7259f694716f34a52b
Devin Yapa <72931352+Devinyapa@users.noreply.github.com>
2023-05-14 14:01:09 +0530

    do some changes

--- app/models/M_Seller_purchase.php
+++ app/models/M_Seller_purchase.php
@@ -7,15 +7,28 @@
     }
 
     
+    // public function get_purchase_list(){
+    //     // $this->db->query('SELECT seller_purchases.purchase_id, seller_purchases.quantity, seller_purchases.date, raw_material.product_name, raw_material.price, raw_material.raw_material_image from seller_purchases inner join raw_material on raw_material.product_id = seller_purchases.product_id where seller_id= :id');
+    //     $this->db->query('SELECT seller_orders.order_id, seller_orders.created_at, seller_order_raw_material.review_status, seller_order_raw_material.quantity, raw_material.product_name, raw_material.price, raw_material.raw_material_image
+    //     FROM seller_orders 
+    //     INNER JOIN seller_order_raw_material ON seller_orders.order_id = seller_order_raw_material.order_id 
+    //     INNER JOIN raw_material ON raw_material.product_id = seller_order_raw_material.product_id 
+    //     WHERE seller_orders.seller_id = :user_id AND seller_orders.current_status = 1
+    //     GROUP BY seller_orders.order_id, seller_orders.created_at, seller_order_raw_material.product_id 
+    //     ORDER BY seller_orders.created_at ');
+    //     $this->db->bind(":user_id", $_SESSION['user_id']);
+    //     return $this->db->resultSet();
+    // }
+
     public function get_purchase_list(){
         // $this->db->query('SELECT seller_purchases.purchase_id, seller_purchases.quantity, seller_purchases.date, raw_material.product_name, raw_material.price, raw_material.raw_material_image from seller_purchases inner join raw_material on raw_material.product_id = seller_purchases.product_id where seller_id= :id');
         $this->db->query('SELECT seller_orders.order_id, seller_orders.created_at, seller_order_raw_material.review_status, seller_order_raw_material.quantity, raw_material.product_name, raw_material.price, raw_material.raw_material_image
         FROM seller_orders 
         INNER JOIN seller_order_raw_material ON seller_orders.order_id = seller_order_raw_material.order_id 
         INNER JOIN raw_material ON raw_material.product_id = seller_order_raw_material.product_id 
-        WHERE seller_orders.seller_id = :user_id AND seller_orders.current_status = 1
+        WHERE seller_orders.seller_id = :user_id 
         GROUP BY seller_orders.order_id, seller_orders.created_at, seller_order_raw_material.product_id 
-        ORDER BY seller_orders.created_at ');
+        ORDER BY seller_orders.created_at ;');
         $this->db->bind(":user_id", $_SESSION['user_id']);
         return $this->db->resultSet();
     }
@@ -54,7 +67,7 @@
 
     public function getSearchAds($userId, $search)
     {
-        $this->db->query("SELECT p.purchase_id, p.quantity, p.date, r.product_name, r.price, r.raw_material_image FROM seller_purchases AS p JOIN raw_material AS r on p.product_id = r.Product_id WHERE r.product_name LIKE '%$search%' AND p.seller_id=$userId"  );
+        $this->db->query("SELECT p.order_id, p.quantity, s.created_at, r.product_name, r.price, r.raw_material_image, p.review_status FROM raw_material AS r JOIN seller_order_raw_material AS p ON p.product_id = r.Product_id JOIN seller_orders AS s ON s.order_id = p.order_id WHERE r.product_name LIKE '%$search%' AND s.seller_id=$userId"  );
         $items = $this->db->resultSet(); 
 
         return array("items" => $items);
